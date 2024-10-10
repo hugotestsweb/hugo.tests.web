8612a949efff828bf128df4bd55814b53a8a9a46
hugotestsweb <hugo.tests.web@gmail.com>
2024-10-10 02:52:54 +0200

    Update finalweb.py

--- finalweb.py
+++ finalweb.py
@@ -1,12 +1,9 @@
 import streamlit as st
-pip install matplotlib
 import matplotlib.pyplot as plt
-import os
-import io
+import requests
 from PIL import Image
 import pandas as pd
 import numpy as np
-import requests
 from io import BytesIO
 
 #FONCTIONS
